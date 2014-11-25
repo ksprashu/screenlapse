f82cb5a396577822e38a01ac65c1afeda1a8c26a
Prashanth K S <prashanth.k.s@sap.com>
2014-11-25 20:48:09 +0530

    updating maps

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var options = {
   renderDelay: 10000
 };
 
-var TRAFFIC_URL = 'http://localhost:8000/trafficmap.html';
-var INTERVAL = 1000;
+var TRAFFIC_URL = 'https://rawgit.com/ksprashu/screenlapse/master/trafficmap_graphite.html';
+var INTERVAL = 60000;
 
 //grabScreen(TRAFFIC_URL, 'traffic01.png');
 
@@ -13,17 +13,6 @@ setInterval(function () {
   var filename = 'traffic_' + Date.now() + '.png';
   grabScreen(TRAFFIC_URL, filename);
 }, INTERVAL);
-
-//fs.readFile('trafficmap.html', {encoding: 'utf-8'}, function (err, data) {
-//  if (err) throw err;
-//  trafficHTML = data;
-//  console.log(trafficHTML);
-//
-//  webshot(trafficHTML, 'traffic.png', options,
-//    function (err) {
-//      if (err) throw err;
-//    });
-//});
 
 function grabScreen (url, fileName) {
   webshot(url, fileName, options,
