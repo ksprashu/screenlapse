257ffdb5dc8b7692ebb703a272367ac8f3d7b5e9
Prashanth K S <prashanth.k.s@sap.com>
2014-11-25 22:30:00 +0530

    phantom on heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var webshot = require('webshot');
 
 var options = {
-  renderDelay: 10000
+  renderDelay: 2000
 };
 
-var TRAFFIC_URL = 'https://rawgit.com/ksprashu/screenlapse/master/trafficmap_graphite.html';
-var INTERVAL = 20000;
+//var TRAFFIC_URL = 'https://cdn.rawgit.com/ksprashu/screenlapse/master/trafficmap_graphite.html';
+var TRAFFIC_URL = 'http://localhost:8000/trafficmap_graphite.html';
+var INTERVAL = 2000;
 
 grabScreen(TRAFFIC_URL, 'traffic01.png');
 
@@ -15,6 +16,7 @@ grabScreen(TRAFFIC_URL, 'traffic01.png');
 //}, INTERVAL);
 
 function grabScreen (url, fileName) {
+  console.log('starting...');
   webshot(url, fileName, options,
     function (err) {
       if (err) throw err;
