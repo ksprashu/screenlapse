7e2b6014b05e5158de62af1e7eacbc0293cc7c85
Prashanth K S <prashanth.k.s@sap.com>
2014-11-25 22:34:00 +0530

    setting phantom path

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var webshot = require('webshot');
 
 var options = {
-  renderDelay: 2000
+  renderDelay: 10000,
+  phantomPath : path.join(__dirname, 'vendor/phantomjs/bin/phantomjs')
 };
 
 //var TRAFFIC_URL = 'https://cdn.rawgit.com/ksprashu/screenlapse/master/trafficmap_graphite.html';
 var TRAFFIC_URL = 'http://localhost:8000/trafficmap_graphite.html';
-var INTERVAL = 2000;
+var INTERVAL = 60000;
 
-grabScreen(TRAFFIC_URL, 'traffic01.png');
+//grabScreen(TRAFFIC_URL, 'traffic01.png');
 
-//setInterval(function () {
-//  var filename = 'traffic_' + Date.now() + '.png';
-//  grabScreen(TRAFFIC_URL, filename);
-//}, INTERVAL);
+setInterval(function () {
+  var filename = 'traffic_' + Date.now() + '.png';
+  grabScreen(TRAFFIC_URL, filename);
+}, INTERVAL);
 
 function grabScreen (url, fileName) {
   console.log('starting...');
