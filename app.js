97a6ec71e0900252d0cd4246ab04d51fe387b2a2
Prashanth K S <prashanth.k.s@sap.com>
2014-11-26 19:44:23 +0530

    remvoed all functions

--- app.js
+++ app.js
@@ -6,25 +6,14 @@ var options = {
   phantomPath : path.join(__dirname, 'vendor/phantomjs/bin/phantomjs')
 };
 
-var TRAFFIC_URL = "https://cryptic-headland-4007.herokuapp.com/trafficmap_graphite.html";
-//var TRAFFIC_URL = 'public/trafficmap_graphite.html';
+var URL = "https://cryptic-headland-4007.herokuapp.com/trafficmap_graphite.html";
 var INTERVAL = 10000;
 
-function screenLapse () {
-  setInterval(function () {
-    var filename = 'traffic_' + Date.now() + '.png';
-    grabScreen(TRAFFIC_URL, filename);
-  }, INTERVAL);
-}
-
-function grabScreen (url, fileName) {
-  console.log('starting...');
-  webshot(url, fileName, options,
+setInterval(function () {
+  var fileName = 'traffic_' + Date.now() + '.png';
+  webshot(URL, fileName, options,
     function (err) {
       if (err) console.log(err);
       console.log(fileName + ' ...saved!');
     });
-}
-
-grabScreen(TRAFFIC_URL, 'traffic01.png');
-//screenLapse();
+}, INTERVAL);
