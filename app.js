dbeda700ace400dbae39bb88c7133eb3bbfdd7b7
Prashanth K S <prashanth.k.s@sap.com>
2014-11-26 22:13:28 +0530

    resetting to heroku phantompath

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 var webshot = require('webshot');
 var path = require('path');
+//var mkdirp = require('mkdirp');
 
 var options = {
   renderDelay: 10000,
   timeout: 20000,
-  //phantomPath : path.join(__dirname, 'vendor/phantomjs/bin/phantomjs')
+  phantomPath: path.join(__dirname, 'vendor/phantomjs/bin/phantomjs')
 };
 
 var URL = "http://cryptic-headland-4007.herokuapp.com/trafficmap_graphite.html";
-URL = "google.com"
-var INTERVAL = 10000;
+var INTERVAL = 60000;
+
+console.log('starting screenlapse...');
 
 setInterval(function () {
-  var fileName = 'traffic_' + Date.now() + '.png';
-  webshot(URL, fileName, options,
-    function (err) {
+  var d = Date.now();
+
+  // create a new folder every day 3am IST == 9:30pm UTC
+
+  //var dirName = d.getUTCFullYear() + '-' + d.getUTCMonth() + '-' + d.getUTCDate();
+  var fileName = 'traffic_' + d + '.png';
+
+
+  webshot(URL, fileName, options, function (err) {
       if (err) console.log(err);
       console.log(fileName + ' ...saved!');
     });
